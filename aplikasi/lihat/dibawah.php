f6290223ca93673a80dd043ef03f9ad72018f623
amin007 <helmi007@yahoo.com>
2015-09-07 21:17:30 +0800

    pindah JS ke dibawah

--- aplikasi/lihat/dibawah.php
+++ aplikasi/lihat/dibawah.php
@@ -1,7 +1,15 @@
-<!--</div> <div id="content"> -->
+
 <footer>
-<span class="label label-info">&copy; Hak Cipta Terperihara 2012</span>
-<h3>Azim Yang Punya</h3>
+<span class="label label-info">&copy; Hak Cipta Terperihara</span>
 </footer>
+<!-- // untuk paparkan JS sahaja -->
+<?php
+if (isset($this->js)) 
+  foreach ($this->js as $js)
+  {
+    echo "\n" . '<script type="text/javascript" src="' . URL . $js .'"></script>';
+  }
+
+?>
 </body>
 </html>
