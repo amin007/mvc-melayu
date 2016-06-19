672a35e4f68ed72811a03b9045d460dfa1afed05
Amin PO Muar <helmi007@yahoo.com>
2016-06-19 13:33:19 +0800

    tukar kepada namespace

--- aplikasi/kawal/sesat.php
+++ aplikasi/kawal/sesat.php
@@ -1,35 +1,22 @@
 <?php
-
-class Sesat extends Kawal 
+namespace aplikasi\kawal; //echo __NAMESPACE__; 
+class Sesat extends \aplikasi\pustaka\Kawal 
 {
-
+#--------------------------------------------------------------------------------------------
 	function __construct() 
 	{
 		parent::__construct();
-		Sesi::init();
-		$logged = Sesi::get('loggedIn');
-		$level = Sesi::get('levelPegawai');
-		// semak level
-		$senaraiLevel=array('fe', 'kawal');
-		
-		if ($logged == false || !in_array($level,$senaraiLevel)) 
-		{
-			Sesi::destroy();
-			header('location:' . URL);
-			exit;
-		}
 	}
-	
+#--------------------------------------------------------------------------------------------	
 	function index() 
 	{
-		// Set pemboleubah utama
-		$this->lihat->pegawai = senarai_kakitangan();
-		$this->lihat->gambar=gambar_latarbelakang('../../');
-		$this->lihat->Tajuk_Muka_Surat='MM 2012';
-		$this->lihat->url = dpt_url();
-		// pergi papar kandungan
-		$this->lihat->mesej = 'Halaman ini tidak wujud';
-		$this->lihat->baca('sesat/index');
-	}
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = 'GPS - Guna Pun Sesat';
+		$this->papar->url = dpt_url();
 
+		# pergi papar kandungan
+		$this->papar->mesej = 'Halaman ini tidak wujud';
+		$this->papar->baca('sesat/gps');
+	}
+#--------------------------------------------------------------------------------------------
 }
