5cacebf178ee03240dec23f5811f495388aaf89f
Amin PO Muar <helmi007@yahoo.com>
2016-06-19 12:24:31 +0800

    bentuk semula mvc melayu guna namespace

--- aplikasi/kawal/index.php
+++ aplikasi/kawal/index.php
@@ -1,35 +1,20 @@
 <?php
-
-class Index extends Kawal 
+namespace aplikasi\pustaka; //echo __NAMESPACE__; 
+class Index extends \aplikasi\pustaka\Kawal 
 {
-
+#--------------------------------------------------------------------------------------------
 	function __construct() 
 	{
 		parent::__construct();
-		/*
-		Sesi::init();
-		$logged = Sesi::get('loggedIn');
-		$level = Sesi::get('levelPegawai');
-		// semak level
-		$senaraiLevel=array('fe', 'kawal');
-		
-		if ($logged == true && in_array($level,$senaraiLevel)) 
-		{
-			header('location:' . URL . 'ruangtamu');
-			exit;
-		}
-		*/
 	}
-	
+#--------------------------------------------------------------------------------------------	
 	function index() 
 	{
-
-		// Set pemboleubah utama
-		$this->lihat->Tajuk_Muka_Surat='SEMAK';
-		//$this->lihat->gambar=gambar_latarbelakang('../../');
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = 'SEMAK';
 		
-		// pergi papar kandungan
-		$this->lihat->baca('index/index');
+		# pergi papar kandungan
+		$this->papar->baca('index/index');
 	}
-	
+#--------------------------------------------------------------------------------------------	
 }
