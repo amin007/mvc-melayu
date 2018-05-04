658b47eb9eeb0f3ac08407a4fb7490613ccb9244
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-04 23:15:59 +0800

    kemaskini koding

--- Aplikasi/Kelas/Utama/Tanya/cari_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/cari_tanya.php
@@ -1,76 +1,219 @@
 <?php
-
-class Cari_Tanya extends Tanya 
+namespace Aplikasi\Tanya; //echo __NAMESPACE__; 
+class Cari_Tanya extends \Aplikasi\Kitab\Tanya
 {
-
-	public function __construct() 
+#=====================================================================================================
+	public function __construct()
 	{
 		parent::__construct();
-		/*
-		$url = dpt_url();
-		echo '<pre>public function __construct() Cari_Tanya extends Tanya<br>$URL->' ,
-			print_r($url , 1) . '</pre>';
-		*/
 	}
-
-	public function paparMedan($myTable)
+#---------------------------------------------------------------------------------------------------#
+	function data_contoh($pilih)
 	{
-		//return $this->db->select('SHOW COLUMNS FROM ' . $myTable);
-		$sql = 'SHOW COLUMNS FROM ' . $myTable;
-		$this->db->select($sql);
-		return $this->db->dpt_data(2,null);
+		$data = array(
+			'namaPendek' => 'james007',
+			'namaPenuh' => 'Polan Bin Polan',
+			'level' => 'pelawat'
+		); # dapatkan medan terlibat
+		$kira = 1; # kira jumlah data
 
+		return ($pilih==1) ? $kira : $data; # pulangkan nilai
 	}
-	
-	public function cariBanyakMedan($myTable, $medan, $kira, $had)
+#---------------------------------------------------------------------------------------------------#
+	function alihMedan()
 	{
-		$sql ="\rSELECT $medan FROM `$myTable` WHERE \r";
-		
-		foreach ($_POST['pilih'] as $key=>$cari)
-		{
-			$apa = $_POST['cari'][$key];
-			$f = isset($_POST['fix'][$key]) ? $_POST['fix'][$key] : null;
-			$atau = isset($_POST['atau'][$key]) ? $_POST['atau'][$key] : null;
-			
-			//$sql.="\r$key => $f  | ";
+		//ALTER TABLE Employees MODIFY COLUMN empName VARCHAR(50) AFTER department;
+	}
+#---------------------------------------------------------------------------------------------------#
+	public function cariPOST($myTable, $medan, $susun)
+	{
+		$sql = 'SELECT ' . $medan . "\r" . ' FROM ' . $myTable . "\r"
+			 . $this->dimanaPOST($myTable) . $susun;
 
-			if ($apa==null) 
-				$sql .= "$atau $cari is null\r";
-			elseif ($myTable=='msic2008') 
+		//echo '<pre>$sql->'; print_r($sql); echo '</pre>';
+		return $this->db->selectAll($sql);
+	}
+#---------------------------------------------------------------------------------------------------#
+	private function dimanaPOST($myTable)
+	{
+		//echo '<pre>$_POST->'; print_r($_POST) . '</pre>';
+		// //' WHERE ' . $medan . ' like %:cariID% ', array(':cariID' => $cariID));
+		$where = null;
+		if($_POST==null || empty($_POST) ):
+			$where .= null;
+		else:
+			foreach ($_POST['pilih'] as $key=>$cari)
 			{
-				if ($cari=='msic') $sql.=($f=='x') ?
-				"$atau ($cari='$apa' or msic2000='$apa')\r" :
-				"$atau ($cari like '%$apa%' or msic2000 like '%$apa%')\r";
-				else $sql.=($f=='x') ?
-				"$atau ($cari='$apa' or notakaki='$apa')\r" :
-				"$atau ($cari like '%$apa%' or notakaki like '%$apa%')\r";
-			}
-			else 
-				$sql.=($f=='x') ? "$atau `$cari`='$apa'\r" : 
-				"$atau `$cari` like '%$apa%'\r";					
-		}
-		
-		$sql.="LIMIT $had ";
-		//echo $sql . '<br>';
-		$this->db->select($sql);
-		return $this->db->dpt_data(2,null);
-	
-	}
-	
-	public function cariSatuSahaja($myTable, $medan, $cari)
-	{
-	
-		$cariMedan = ( !isset($cari['medan']) ) ? '' : $cari['medan'];
-		$cariID = ( !isset($cari['id']) ) ? '' : $cari['id'];
-		
-		$sql = 'SELECT ' . $medan . ' FROM ' . 	$myTable . 
-			' WHERE ' . $cariMedan . ' = "' . $cariID . '" ';
-		
-		//echo $sql . '<br>';
-		$result = $this->db->select($sql);
-		//echo json_encode($result);
-		
-		return $result;
-	}
+				$apa = $_POST['cari'][$key];
+				$f = isset($_POST['fix'][$key]) ? $_POST['fix'][$key] : null;
+				$atau = isset($_POST['atau'][$key]) ? $_POST['atau'][$key] : 'WHERE';
 
+				//$sql.="\r$key => $f  | ";
+
+				if ($apa==null)
+					$where .= " $atau `$cari` is null\r";
+				elseif ($myTable=='msic2008')
+				{
+					if ($cari=='msic') $where .= ($f=='x') ?
+					" $atau (`$cari`='$apa' OR msic2000='$apa')\r" :
+					" $atau (`$cari` like '%$apa%' OR msic2000 like '%$apa%')\r";
+					else $where .= ($f=='x') ?
+					" $atau (`$cari`='$apa' OR notakaki='$apa')\r" :
+					" $atau (`$cari` like '%$apa%' OR notakaki like '%$apa%')\r";
+				}
+				else
+					$where .= ($f=='x') ? " $atau `$cari`='$apa'\r" :
+					" $atau `$cari` like '%$apa%'\r";
+			}
+		endif;
+
+		return $where;
+	} // private function dimanaPOST()
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+	function medanIndustri($myTable)
+	{
+		if ($myTable=='msic2008'):
+			$medan = 'seksyen S,bahagian B,kumpulan Kpl,kelas Kls,'
+			. 'msic2000,msic,keterangan,notakaki';
+		elseif ($myTable=='msic_v1'):
+			$medan = 'survey kp,msic,keterangan,notakaki';
+		else : $medan = '*';
+		endif;
+
+		return $medan;
+	}
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+	function bentukPembolehubah($post, $key, $m0)
+	{
+		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		$fx = isset($post['fix'][$key]) ? $post['fix'][$key] : null;
+		$f = ($fx=='x') ? 'or(x=)' : 'or(%like%)';
+		$at = isset($post['atau'][$key]) ? $post['atau'][$key] : 'WHERE';
+		$m1 = $m0 . '|msic2000'; $m2 = $m0 . '|notakaki';
+		$apa = isset($post['cari'][$key]) ? $post['cari'][$key] : null;
+
+		return array($f, $at, $m1, $m2, $apa);
+	}
+#---------------------------------------------------------------------------------------------------#
+	function bentukCarian($post, $myTable)
+	{	//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		//echo '<pre>$post->'; print_r($post); echo '</pre>';
+		$carian = null; //' WHERE ' . $medan . ' like %:cariID% ', array(':cariID' => $cariID));
+		if($_POST==null || empty($_POST) ):
+			$carian .= null;
+		else:
+			foreach ($post['pilih'] as $key=>$cari)
+			{	//echo "\r$key => $cari ($myTable)| ";
+				$carian[] = $this->bentukCarian2($myTable, $post, $key, $cari);
+			}
+		endif; //echo '<pre>$carian->'; print_r($carian); echo '</pre>';
+
+		return $carian;//*/
+	}
+#---------------------------------------------------------------------------------------------------#
+	function bentukCarian1($myTable, $post, $key, $m0)
+	{
+		list($f1, $at, $m1, $m2, $apa) = $this->bentukPembolehubah($post, $key, $m0);
+		if($myTable=='msic2008')
+		{
+			if ($m0=='msic')
+				$carian = array('fix'=>$f1,'atau'=>$at,'medan'=>$m1,'apa'=>$apa);
+			else
+				$carian = array('fix'=>$f1,'atau'=>$at,'medan'=>$m2,'apa'=>$apa);
+		}
+		else
+		{
+			$carian = array('fix'=>'%like%','atau'=>$at,'medan'=>$m0,'apa'=>$apa);
+		}//*/
+		return $carian;
+	}
+#---------------------------------------------------------------------------------------------------#
+	function bentukCarian2($myTable, $post, $key, $m0)
+	{
+		list($f1, $at, $m1, $m2, $apa) = $this->bentukPembolehubah($post, $key, $m0);
+		$carian = ($myTable=='msic2008') ?
+		( ($m0=='msic') ?
+			array('fix'=>$f1,'atau'=>$at,'medan'=>$m1,'apa'=>$apa)
+			: array('fix'=>$f1,'atau'=>$at,'medan'=>$m2,'apa'=>$apa) )
+			: array('fix'=>'%like%','atau'=>$at,'medan'=>$m0,'apa'=>$apa);
+		return $carian;
+	}
+#---------------------------------------------------------------------------------------------------#
+	function bentukCarian0($myTable, $post, $key, $m0)
+	{
+		echo '<br>$myTable : ' . $myTable;
+		list($f1, $at, $m1, $m2, $apa) = $this->bentukPembolehubah($post, $key, $m0);
+		echo '<br>$f1 : ' . $f1; echo '<br>$at : ' . $at;
+		echo '<br>$m0 : ' . $m0 . '<br>$m1 : ' . $m1 . '<br>$m2 : ' . $m2;
+		echo '<br>$apa : ' . $apa;
+		echo '<br>';
+	}
+#---------------------------------------------------------------------------------------------------#
+	function jadualDataCorp()
+	{
+		/*$jadual = array('`aes`','`kawalan_aes`','`aes_alam_sekitar`',
+		'`aes_kp_205`','`aes_kp_206`','`aes_kp_207`','`aes_kp_800`',
+		'`aes_perkhidmatan`','`aes_pertanian`');//*/
+		$jadual = array('aes','kawalan_aes','aes_alam_sekitar',
+		'aes_kp_205','aes_kp_206','aes_kp_207','aes_kp_800',
+		'aes_perkhidmatan','aes_pertanian');
+		$medan = '*';
+		# cari id berasaskan newss/ssm/sidap/nama
+		//$id['nama'] = bersih(isset($_POST['cari']) ? $_POST['cari'] : null);
+
+		return array($jadual,$medan);
+	}
+#---------------------------------------------------------------------------------------------------#
+	function dataCorp($apa)
+	{
+		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		//echo '<pre>$apa->'; print_r($apa); echo '</pre>';
+		$carian = null; 
+		if($_POST==null || empty($_POST) ):
+			$carian .= null;
+		else:
+			list($jadual, $medan) = $this->jadualDataCorp();
+			$carian[] = array('fix'=>'z%like%','atau'=>'WHERE',
+				'medan' => 'concat_ws("",newss,nossm,nama)',
+				'apa' => $apa[1]);
+		endif;
+
+		//echo '<pre>$jadual->'; print_r($jadual); echo '</pre>';
+		//echo '<pre>$medan->'; print_r($medan); echo '</pre>';
+		//echo '<pre>$carian->'; print_r($carian); echo '</pre>';
+
+		return array($jadual, $medan, $carian);
+	}
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+	function bentukMedanJohor()
+	{
+		//`KOD NEGERI`, `NEGERI`,
+		//echo '6)$namajadual=' . $namajadual . '<br>';
+		# senarai nama medan
+		$medanAsal = '`KOD NGDBBP 2010`,`PEJABAT OPERASI`,' .
+		"\r" . ' concat(`KOD DAERAH BANCI`,"-",`DAERAH BANCI`," | ",`NEGERI`) as DB,' .
+		"\r" . ' concat(`KOD STRATA`,"-",`STRATA`) as STRATA,' .
+		"\r" . ' concat(`KOD MUKIM`,"-",`MUKIM`) as MUKIM,' .
+		"\r" . ' concat(`KOD BP`,"-",`DAERAH PENTADBIRAN`) as DAERAH,' .
+		"\r" . ' concat(`KOD PBT`,"-",`PIHAK BERKUASA TEMPATAN`) as PBT,' .
+		"\r" . ' concat(`KOD BDR`,"-",`NAMA BANDAR`) as BANDAR,' .
+		"\r" . '`DESKRIPSI (LOKALITI STATISTIC KAWKECIL)`, `LOKALITI UNTUK INDEKS`';
+		# senarai nama medan
+		$medanBaru = '`KOD NGDBBP 2010`,' .
+		//"\r" . ' concat("01",`no_db`, `no_bp_baru`) as `KodNGDBBP`,' .
+		"\r" . ' `kod_strata` as STRATA, NEGERI,' .
+		"\r" . ' concat(`KodMukim`,"-",`Mukim`) as MUKIM,' .
+		"\r" . ' concat(`KodDP`,"-",`Daerah Pentadbiran`) as DAERAH,' .
+		"\r" . ' concat(`KodPBT`,"-",`PBT`) as PBT,' .
+		"\r" . ' `catatan`, `kawasan`,' .
+		"\r" . ' `LOKALITI UNTUK INDEKS`';
+
+		return array($medanAsal, $medanBaru);
+	}
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#=====================================================================================================
 }
