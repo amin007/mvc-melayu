fd6ff913a1a93a04cf6c45bf585586fdd8e07f4f
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-04 22:51:44 +0800

    alih ke dalam Biarahsia

--- Aplikasi/Kelas/Kitab/Biarahsia/Sesi.php
+++ Aplikasi/Kelas/Kitab/Biarahsia/Sesi.php
@@ -1,28 +1,28 @@
 <?php
-namespace aplikasi\pustaka; //echo __NAMESPACE__; 
-class Sesi
+namespace Aplikasi\Kitab; //echo __NAMESPACE__; 
+class Sesi # \Aplikasi\Kitab\Sesi::
 {
-	
+#==========================================================
 	public static function init()
 	{
 		@session_start();
 	}
-	
+
 	public static function set($key, $value)
 	{
 		$_SESSION[$key] = $value;
 	}
-	
+
 	public static function get($key)
 	{
 		if (isset($_SESSION[$key]))
 		return $_SESSION[$key];
 	}
-	
+
 	public static function destroy()
 	{
 		//unset($_SESSION);
 		session_destroy();
 	}
-	
+#==========================================================
 }
