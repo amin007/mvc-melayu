859c5e11eae1fec4601ec5033bcd978b104b34e2
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-30 23:47:30 +0800

    tambah untuj testing

--- Aplikasi/Fail/Papar/index/index.php
+++ Aplikasi/Fail/Papar/index/index.php
@@ -1,3 +1,3 @@
 <h1>Ini fail utama</h1>
 <a href="<?php echo URL ?>docs/">Klik sini untuk panduan utama</a><br>
-<a href="<?php echo URL ?>ayam/goreng/mcd/kfc/">Klik sini untuk test url</a>
+<a href="<?php echo URL ?>ayam/goreng/mcd/kfc/abc/123/">Klik sini untuk test url</a>
