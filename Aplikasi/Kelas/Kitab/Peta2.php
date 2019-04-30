b91e964faa9bf07f10ccefea270decfa547b5ba9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-30 15:15:58 +0800

    membina konsep baru dalam mvc-melayu

---
+++ Aplikasi/Kelas/Kitab/Peta2.php
@@ -0,0 +1,76 @@
+<?php
+/* Ini class untuk
+ * 1. membaca $url dari fungsi dpt_url() dari fail fungsi.php
+ *    dan masukkan dalam $url
+ * 2. semak sama ada nilai $url[0] kosong tak
+ * 3. dapatkan fail dalam folder KAWAL yang serupa dengan $url[0]
+ * 4. semak sama ada dalam folder KAWAL $fail benar2 wujud
+ */
+namespace Aplikasi\Kitab; //echo __NAMESPACE__;
+class Peta2
+{
+#==========================================================================================
+#------------------------------------------------------------------------------------------
+	protected $kawal = 'Index';
+	protected $method = 'index';
+	protected $params = [];
+#------------------------------------------------------------------------------------------
+	public function semakPembolehubah($senarai,$jadual,$p='0')
+	{
+		echo '<pre>$' . $jadual . '=><br>';
+		if($p == '0') print_r($senarai);
+		if($p == '1') var_export($senarai);
+		if($p == '2') var_dump($senarai);
+		echo '</pre>';//*/
+		//$this->semakPembolehubah($ujian,'ujian',0);
+		#http://php.net/manual/en/function.var-export.php
+		#http://php.net/manual/en/function.print-r.php
+	}
+#------------------------------------------------------------------------------------------
+	public function parseURL()
+	{
+		if( isset($_GET['url']) )
+		{
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
+	}
+#------------------------------------------------------------------------------------------
+	public function __construct()
+	{
+		# 1. capai fungsi dpt_url() dan masukkan dalam $url
+		$url = $this->parseURL();$this->semakPembolehubah($url,'url');
+
+		/*# controller
+		if( file_exists('app/controllers/' . $url[0] . '.php') )
+		{
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+		require_once 'app/controllers/' . $this->controller . '.php';
+		$this->controller = new $this->controller;
+
+		# method
+		if( isset($url[1]) )
+		{
+			if( method_exists($this->controller, $url[1]) )
+			{
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		# params
+		if ( !empty($url) )
+		{
+			$this->params = array_values($url);
+		}
+
+		# jalankan controller & method, serta kirim params jika ada
+		call_user_func_array([$this->controller,$this->method], $this->params);*/
+	}
+#------------------------------------------------------------------------------------------
+#==========================================================================================
+}
