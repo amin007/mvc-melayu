fef46d5ddf2482e815a4c3ff54679b97bed4b0ba
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-03 11:35:29 +0800

    tukar link ke panduan utama

--- Aplikasi/Fail/Papar/index/index.php
+++ Aplikasi/Fail/Papar/index/index.php
@@ -1,3 +1,3 @@
 <h1>Ini fail utama</h1>
-<a href="<?php echo URL ?>docs/">Klik sini untuk panduan utama</a><br>
+<a href="<?php echo URL ?>docs/i-persembahan.html">Klik sini untuk panduan utama</a><br>
 <a href="<?php echo URL ?>ayam/goreng/mcd/kfc/abc/123/">Klik sini untuk test url</a>
